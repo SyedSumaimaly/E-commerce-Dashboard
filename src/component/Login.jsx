99893b91cecde85d40a1abb0135348e9946a5c3d
mbilal-09 <bilalkhanshakir@gmail.com>
2024-03-14 10:29:29 +0500

    product page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
-import { Button, Label, TextInput } from 'flowbite-react';
+import React from "react";
+import { Button, Label, TextInput } from "flowbite-react";
 
 function Login() {
-    return (
-        <>
-            <div className='h-screen w-full flex  justify-center items-center'>
-                <form className="flex flex-col bg-blue-600 gap-4 p-4 rounded-lg w-1/2">
-                    <div className='font-bold text-xl'>
-                        Dashboard Login
-                    </div>
-                    <div>
-                        <div className="mb-2 block">
-                            <Label htmlFor="email1" value="Your Email" />
-                        </div>
-                        <TextInput id="email1" type="email" placeholder="Enter your email" required />
-                    </div>
-                    <div>
-                        <div className="mb-2 block">
-                            <Label htmlFor="password1" value="Your Password" />
-                        </div>
-                        <TextInput id="password1" type="password" required />
-                    </div>
-                    <Button type="submit" className='bg-white text-blue-600 hover:bg-white'>Submit</Button>
-                </form>
+  return (
+    <>
+      <div className="h-screen w-full flex  justify-center items-center bg-blue-700">
+        <form className="flex flex-col bg-white gap-4 p-4 rounded-lg w-1/2">
+          <div className="font-bold text-xl">Dashboard Login</div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email1" value="Your Email" />
             </div>
-        </>
-    )
+            <TextInput
+              id="email1"
+              type="email"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password1" value="Your Password" />
+            </div>
+            <TextInput id="password1" type="password" required />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-700 text-white p-2 rounded-lg hover:bg-white hover:text-blue-700"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
